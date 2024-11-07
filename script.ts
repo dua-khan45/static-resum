272a33449efba08c29cd202577b599d0f89e493a
dua-khan45 <dua40zadi@gmail.com>
2024-11-07 14:52:23 +0500

    push my code

---
+++ script.ts
@@ -0,0 +1,11 @@
+// const toggleButton = document.getElementById(`toggle-skills`) as HTMLButtonElement
+// Const skills  = document.getElementById(`skills`) as HTMLElement
+//  toggleButton.addEventListener(`click`, ()=> {
+//     if(skills.stlye.display ===`none`) {
+//         skills.style.display = `block`
+//      }else {
+//         skills.style.display = `none`
+//      }
+//  })
+// const toggleButton = document.getElementById(`toggle-skills`) as HTMLButtonElement
+// const 
